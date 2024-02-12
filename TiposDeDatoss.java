528728b172f67f2658096b6797d53144d1990c2c
AlexisBurci <159494505+AlexisBurci@users.noreply.github.com>
2024-02-12 16:51:38 -0600

    Add files via upload

---
+++ TiposDeDatoss.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tipos.de.datos;
+
+/**
+ *
+ * @author invitado
+ */
+public class TiposDeDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        byte pruebaByte; // 1 byte = 8 bits 2 - 256
+        pruebaByte = -127;
+        pruebaByte = 127;
+        
+        short pruebaShort;
+        pruebaShort = -32768;
+        pruebaShort = 32767;
+        System.out.println(pruebaShort);
+        pruebaShort++;
+        System.out.println(pruebaShort);
+        
+                
+    }
+    
+}
