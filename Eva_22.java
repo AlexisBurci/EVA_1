49387f08cd2b8173dc962707766a4430c7604dfe
AlexisBurci <159494505+AlexisBurci@users.noreply.github.com>
2024-02-12 16:50:46 -0600

    Add files via upload

---
+++ Eva_22.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva_2;
+
+/**
+ *
+ * @author invitado
+ */
+public class Eva_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+int edad;
+String nombrePropio;
+double salario;
+edad = 50;
+nombrePropio = "AlexisDavid";
+salario=123.3;
+
+System.out.println(nombrePropio);
+System.out.println(edad);
+System.out.println(salario);
+    }
+    
+}
